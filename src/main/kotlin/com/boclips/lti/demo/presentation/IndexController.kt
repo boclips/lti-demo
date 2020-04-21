e779011121b72b1a6e8680aa1471e4ef90d82292
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2020-04-21 15:16:50 +0200

    Provide a form that allows posting initiate login request to iframe
    [#172144229]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/main/kotlin/com/boclips/lti/demo/presentation/IndexController.kt
+++ src/main/kotlin/com/boclips/lti/demo/presentation/IndexController.kt
@@ -1,10 +1,16 @@
 package com.boclips.lti.demo.presentation
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class IndexController {
+class IndexController(@Value("\${boclips.lti.initiateLoginUrl}") private val initiateLoginUrl: String) {
     @GetMapping("/")
-    fun landingPage() = "index"
+    fun landingPage() = ModelAndView(
+        "index", mapOf(
+            "initiateLoginUrl" to initiateLoginUrl
+        )
+    )
 }
--- src/test/kotlin/com/boclips/lti/demo/presentation/IndexControllerIntegrationTest.kt
+++ src/test/kotlin/com/boclips/lti/demo/presentation/IndexControllerIntegrationTest.kt
@@ -1,27 +1,25 @@
 package com.boclips.lti.demo.presentation
 
 import com.boclips.lti.demo.testsupport.AbstractSpringIntegrationTest
-import org.hamcrest.Matchers.containsString
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
 
+@TestPropertySource(
+    properties = ["boclips.lti.initiateLoginUrl=http://localhost:8080/v1p3/initiate-login"]
+)
 class IndexControllerIntegrationTest : AbstractSpringIntegrationTest() {
     @Test
-    fun `renders a landing page`() {
+    fun `renders a landing with expected model parameters`() {
         mvc.perform(get("/"))
             .andExpect(status().isOk)
             .andExpect(view().name("index"))
-            .andExpect(
-                content().string(
-                    containsString(
-                        """
-                            <a href="https://google.com">Google</a>
-                        """.trimIndent()
-                    )
-                )
-            )
+            .andExpect(model().attribute("initiateLoginUrl", "http://localhost:8080/v1p3/initiate-login"))
+            .andExpect(content().string(containsString("http://localhost:8080/v1p3/initiate-login")))
     }
 }
